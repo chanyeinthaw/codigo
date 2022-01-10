47e111400c29cb4c0044077d33dbd95fa60f071b
Chan Nyein Thaw <me@chanyeinthaw.com>
2022-01-11 02:00:36 +0630

    feat: add Headline component

---
+++ pages/work/components/Headline/Headline.js
@@ -0,0 +1,9 @@
+import css from "./Headline.module.scss";
+
+export default function Headline() {
+    return <section className={css.headline}>
+        <p>Here’s 5% of</p>
+        <p>our published work.</p>
+        <p>See 100% of our power.</p>
+    </section>
+}
