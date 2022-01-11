1bd15258190f2c06604c6266a973fd4c13aedbfe
Chan Nyein Thaw <me@chanyeinthaw.com>
2022-01-11 21:08:19 +0630

    feat: add work api

---
+++ pages/api/work.api.js
@@ -0,0 +1,237 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+let workApi = [
+  {
+    "name": "OCBC Pay Anyone",
+    "slug": "ocbc-pay-anyone",
+    "text_colour": "",
+    "grid_size": 0.5,
+    "description": "You can now pay kopitiam uncles, merchants, peers, and dears with the OCBC Pay Anyone app, and you can even withdraw cash by scanning the QR code at OCBC’s ATMs without your card!",
+    "categories": [
+      {
+        "term_id": 35,
+        "name": "Banking &amp; Finance",
+        "slug": "banking-finance"
+      }
+    ],
+    "roles": [
+      {
+        "term_id": 21,
+        "name": "app",
+        "slug": "app"
+      },
+      {
+        "term_id": 24,
+        "name": "uiux",
+        "slug": "uiux"
+      }
+    ],
+    "images": {
+      "normal": "https://cdn.codigo.co/uploads/2021/04/PAO-0@2x.jpg",
+      "retina": "https://cdn.codigo.co/uploads/2021/04/PAO-0@2x.jpg"
+    }
+  },
+  {
+    "name": "Ecolab",
+    "slug": "ecolab",
+    "text_colour": "",
+    "grid_size": 0.25,
+    "description": "Leads generation iPad app that sales representatives use when conducting surveys at customer’s restaurants.\r\n\r\nBased on the environment and survey results, the pad will recommend suitable cleaning products that customers can use, and the potential savings it comes with it! Results are nicely formatted into a PDF that gets sent to the customer’s inbox with a direct link to purchase products recommended.",
+    "categories": [
+      {
+        "term_id": 29,
+        "name": "Others",
+        "slug": "others"
+      }
+    ],
+    "roles": [
+      {
+        "term_id": 21,
+        "name": "app",
+        "slug": "app"
+      },
+      {
+        "term_id": 23,
+        "name": "cms",
+        "slug": "cms"
+      },
+      {
+        "term_id": 24,
+        "name": "uiux",
+        "slug": "uiux"
+      }
+    ],
+    "images": {
+      "normal": "https://cdn.codigo.co/uploads/2021/04/ECO-0@2x.jpg",
+      "retina": "https://cdn.codigo.co/uploads/2021/04/ECO-0@2x.jpg"
+    }
+  },
+  {
+    "name": "KIWI Auto Marketplace",
+    "slug": "kiwi",
+    "text_colour": "",
+    "grid_size": 0.25,
+    "description": "Myanmar newest 2nd hand car buying, bidding and selling marketplace! You can even add an insurance plan when you buy a car and have the platform settle all the mundane paperwork while you sit back and collect your car when it’s ready.",
+    "categories": [
+      {
+        "term_id": 29,
+        "name": "Others",
+        "slug": "others"
+      },
+      {
+        "term_id": 30,
+        "name": "Start-ups",
+        "slug": "start-ups"
+      }
+    ],
+    "roles": [
+      {
+        "term_id": 21,
+        "name": "app",
+        "slug": "app"
+      },
+      {
+        "term_id": 22,
+        "name": "web",
+        "slug": "web"
+      },
+      {
+        "term_id": 23,
+        "name": "cms",
+        "slug": "cms"
+      },
+      {
+        "term_id": 24,
+        "name": "uiux",
+        "slug": "uiux"
+      }
+    ],
+    "images": {
+      "normal": "https://cdn.codigo.co/uploads/2021/04/KIWI-0@2x.jpg",
+      "retina": "https://cdn.codigo.co/uploads/2021/04/KIWI-0@2x.jpg"
+    }
+  },
+  {
+    "name": "Furama Hotels",
+    "slug": "furama",
+    "text_colour": "",
+    "grid_size": 0.25,
+    "description": "Book, earn, and redeem with the Furama mobile app. Enjoy app-exclusive rates, on top of other discounts and offers. Be a part of the FURAMAfirst membership and earn FURAMAmints with every booking.",
+    "categories": [
+      {
+        "term_id": 26,
+        "name": "Lifestyle",
+        "slug": "lifestyle"
+      }
+    ],
+    "roles": [
+      {
+        "term_id": 21,
+        "name": "app",
+        "slug": "app"
+      },
+      {
+        "term_id": 24,
+        "name": "uiux",
+        "slug": "uiux"
+      }
+    ],
+    "images": {
+      "normal": "https://cdn.codigo.co/uploads/2021/04/FURAMA-0@2x.jpg",
+      "retina": "https://cdn.codigo.co/uploads/2021/04/FURAMA-0@2x.jpg"
+    }
+  },
+  {
+    "name": "Woodlands Transport",
+    "slug": "woodlands-transport",
+    "text_colour": "",
+    "grid_size": 0.25,
+    "description": "Full end-to-end digitalization of WTS school bus operations in Singapore. Parents can now sign their kids for bus services via an app and be notified when their kids board and alight the buses. WTS administrators can track where all their buses are, how many children are on the bus, and how timely each stop is! Using Bluetooth technology, schools are able to detect how on time each bus is arriving and departing the schools daily and be able to optimize route planning. There’s even finance, customer service, sub-contractor management module, the list goes on!",
+    "categories": [
+      {
+        "term_id": 25,
+        "name": "Transport &amp; Logistics",
+        "slug": "transport-and-logistics"
+      }
+    ],
+    "roles": [
+      {
+        "term_id": 21,
+        "name": "app",
+        "slug": "app"
+      },
+      {
+        "term_id": 22,
+        "name": "web",
+        "slug": "web"
+      },
+      {
+        "term_id": 23,
+        "name": "cms",
+        "slug": "cms"
+      },
+      {
+        "term_id": 24,
+        "name": "uiux",
+        "slug": "uiux"
+      }
+    ],
+    "images": {
+      "normal": "https://cdn.codigo.co/uploads/2021/04/WTS-0@2x.jpg",
+      "retina": "https://cdn.codigo.co/uploads/2021/04/WTS-0@2x.jpg"
+    }
+  },
+  {
+    "name": "Ready To Travel (SATS)",
+    "slug": "ready-to-travel",
+    "text_colour": "#000000",
+    "grid_size": 0.5,
+    "description": "The best travel app with a collaborative trip planner so that you and your loved ones and friends can plan trips together! Grab travel essentials like Pocket WiFi, Travel Insurance, and instant access to more than 150 airport lounges all over the world. It even comes with a built-in currency converter, weather forecast, and offline mode!",
+    "categories": [
+      {
+        "term_id": 26,
+        "name": "Lifestyle",
+        "slug": "lifestyle"
+      },
+      {
+        "term_id": 28,
+        "name": "Retail &amp; Entertainment",
+        "slug": "retail-and-entertainment"
+      },
+      {
+        "term_id": 30,
+        "name": "Start-ups",
+        "slug": "start-ups"
+      }
+    ],
+    "roles": [
+      {
+        "term_id": 21,
+        "name": "app",
+        "slug": "app"
+      },
+      {
+        "term_id": 22,
+        "name": "web",
+        "slug": "web"
+      },
+      {
+        "term_id": 23,
+        "name": "cms",
+        "slug": "cms"
+      },
+      {
+        "term_id": 24,
+        "name": "uiux",
+        "slug": "uiux"
+      }
+    ],
+    "images": {
+      "normal": "https://cdn.codigo.co/uploads/2021/04/RTT-0@2x.jpg",
+      "retina": "https://cdn.codigo.co/uploads/2021/04/RTT-0@2x.jpg"
+    }
+  }
+]
+
+export default function handler(req, res) {
+  res.status(200).json({ works: workApi })
+}
