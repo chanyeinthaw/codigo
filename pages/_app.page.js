da6d31df7d4535bb064ab6d02519c2aa0eea4130
Chan Nyein Thaw <me@chanyeinthaw.com>
2022-01-13 09:58:45 +0630

    feat: render nav variant based on route

--- pages/_app.page.js
+++ pages/_app.page.js
@@ -1,13 +1,15 @@
 import '../styles/globals.scss'
 import Nav from "../components/Nav/Nav";
+import {useRouter} from "next/router";
 
-function MyApp({ Component, pageProps }) {
-  return (
-      <main style={{ }}>
-        <Nav variant={'main'}/>
-        <Component {...pageProps} />
-      </main>
-  )
+function MyApp({Component, pageProps}) {
+    let router = useRouter()
+    return (
+        <main style={{}}>
+            <Nav variant={router.pathname === '/work' ? 'main' : 'secondary'}/>
+            <Component {...pageProps} />
+        </main>
+    )
 }
 
 export default MyApp
